da61990c43f9334bfaf22797ae14363691e6fd82
rawan <roooony2030@gmail.com>
2019-11-17 10:51:07 -0800

    Add makeGrid function to draw and color cells

--- designs.js
+++ designs.js
@@ -7,3 +7,27 @@ form_submit.addEventListener('submit', function(event) {
     event.preventDefault();
     makeGrid();
 })
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid() {
+    //get height and width from user 
+    var table_height = document.getElementById('inputHeight').value;
+    var table_width = document.getElementById('inputWidth').value;
+    table.innerHTML = "";
+    var x = 0;
+    while (x < table_height) {
+        var row = document.createElement('tr');
+        table.append(row);
+
+        for (var y = 0; y < table_width; y++) {
+            var cell = document.createElement('td');
+            row.append(cell);
+
+            cell.addEventListener('click', function() {
+                var color = document.getElementById('colorPicker').value;
+                this.style.backgroundColor = color;
+            })
+        }
+        x++;
+    }
+}
