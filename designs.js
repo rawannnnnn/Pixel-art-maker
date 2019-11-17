d65a06ec7a6165a1052c69271c580348351a9804
rawan <roooony2030@gmail.com>
2019-11-17 10:47:54 -0800

    Add event listener to submit button

--- designs.js
+++ designs.js
@@ -2,3 +2,8 @@
 const form_submit = document.getElementById('sizePicker')
 const table = document.getElementById('pixelCanvas');
 
+// add event listener to submit button
+form_submit.addEventListener('submit', function(event) {
+    event.preventDefault();
+    makeGrid();
+})
